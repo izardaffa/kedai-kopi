ae3ef931ffbdf46178037d22d871602eb0751c7f
Nizar Daffa <nizartokuzet@gmail.com>
2023-06-29 11:40:21 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// toggle sidebar
+const navbarNav = document.querySelector('.navbar-nav')
+
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active')
+}
+
+// close sidebar
+const hamburger = document.querySelector('#hamburger-menu')
+
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active')
+    }
+})
